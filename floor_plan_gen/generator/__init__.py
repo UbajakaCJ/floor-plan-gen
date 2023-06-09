90197e394af83283353ef1035f1f967844682553
Chijioke Ubajaka <110869413+UbajakaCJ@users.noreply.github.com>
2023-06-09 12:05:28 +0100

    Updated floor_plan_gen

---
+++ floor_plan_gen/generator/__init__.py
@@ -0,0 +1,3 @@
+from floorgen.generator.BiCycleGAN.BiCycleGANGenerator import BiCycleGANGenerator
+from floorgen.generator.BiCycleGAN.SingleStepGenerator import SingleStepGenerator
+from floorgen.generator.Pix2Pix.Pix2PixGenerator import Pix2PixGenerator
